9d3bcebcd447f639d95db6d5b8934aff03dc812c
Marvin Said <marvin@gmail.com>
2025-10-22 22:57:39 -0400

    Primer commit de mi proyecto Terraform

---
+++ .terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "4.23.0"
+  constraints = "4.23.0"
+  hashes = [
+    "h1:mCTn9CigpMcRazyyg86vZPoGP0+qdchWQQ+K6JL4f44=",
+    "zh:08d950618a2bf14445171a0eae3721b69be731d6732fad72e902e56e49d4d3b0",
+    "zh:1a9c84376a30cc890830d433ec21057417c012135b40af672dc915010160595a",
+    "zh:2db0422d03840b078a03a33599bbd1f6ea9569bf38ef2a2b99e6bcfc65e14400",
+    "zh:35884b51dcf73acc5fcc0ea22414226e4c3709798fac8948239d8601bfd23592",
+    "zh:5d84f4f588b478bbb7f4f3a943d722c9c14177b8003347b2d29978271bc19632",
+    "zh:5e232ba768cf8d7eb0adcf599f93cd89253f9c2af8e22160451ba6e0c7799101",
+    "zh:80807af8909c00df853c13b9cf363ba31ce013733ef353ac206f81033a1747fa",
+    "zh:8ced180e4377e5cd8a5138ec7eb64493ace692f5e5aa8bb5681154f431232f8a",
+    "zh:bd5b5cbbd0b38b91217251c1d977410bc930f11063e4e97c798c834dc00ef369",
+    "zh:c021b1ecf0a3c02bbb56d8e8ac14bdde04e9c1e5b2346563b53450aea468847e",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:fc036967ce8f8322c91e93b20af0b68512df177018e975d742a91445d6b94b37",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.7.2"
+  hashes = [
+    "h1:Def/iHM4HihJCIxQ8AYoxtoVL5lVlYx0V7bX91pxwgM=",
+    "zh:14829603a32e4bc4d05062f059e545a91e27ff033756b48afbae6b3c835f508f",
+    "zh:1527fb07d9fea400d70e9e6eb4a2b918d5060d604749b6f1c361518e7da546dc",
+    "zh:1e86bcd7ebec85ba336b423ba1db046aeaa3c0e5f921039b3f1a6fc2f978feab",
+    "zh:24536dec8bde66753f4b4030b8f3ef43c196d69cccbea1c382d01b222478c7a3",
+    "zh:29f1786486759fad9b0ce4fdfbbfece9343ad47cd50119045075e05afe49d212",
+    "zh:4d701e978c2dd8604ba1ce962b047607701e65c078cb22e97171513e9e57491f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7b8434212eef0f8c83f5a90c6d76feaf850f6502b61b53c329e85b3b281cba34",
+    "zh:ac8a23c212258b7976e1621275e3af7099e7e4a3d4478cf8d5d2a27f3bc3e967",
+    "zh:b516ca74431f3df4c6cf90ddcdb4042c626e026317a33c53f0b445a3d93b720d",
+    "zh:dc76e4326aec2490c1600d6871a95e78f9050f9ce427c71707ea412a2f2f1a62",
+    "zh:eac7b63e86c749c7d48f527671c7aee5b4e26c10be6ad7232d6860167f99dbb0",
+  ]
+}
--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
-  terraform {
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=4.23.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.5"
     }
   }
 }
@@ -16,20 +20,26 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
-# grupo de recursos
-resource "azurerm_resource_group" "rg" {
-  name     = "gr-sisinfo-entel-13893075"
-  location = "Brazil South"
+# Genera un número aleatorio para evitar conflictos
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
 }
 
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "gr-sisinfo-entel-${random_integer.suffix.result}"
+  location = "chilecentral"
+}
+
+# Storage Account
 resource "azurerm_storage_account" "sa" {
-  name                     = "saucbenteldelta13893075"
+  name                     = "saucbenteldelta${random_integer.suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind             = "StorageV2"
-  is_hns_enabled           = "true"
+  is_hns_enabled           = true
 }
 
 resource "azurerm_storage_container" "raw_entel" {
@@ -38,12 +48,13 @@ resource "azurerm_storage_container" "raw_entel" {
   container_access_type = "blob"
 }
 
+# Blobs (usa rutas absolutas de Windows)
 resource "azurerm_storage_blob" "_03csv" {
   name                   = "03.csv"
   storage_account_name   = azurerm_storage_account.sa.name
   storage_container_name = azurerm_storage_container.raw_entel.name
   type                   = "Block"
-  source                 = "Dataset/03.csv"
+  source                 = "C:/Users/MARVIN/Documents/terraform-sisinfo2-clean/Dataset/03.csv"
 }
 
 resource "azurerm_storage_blob" "_05csv" {
@@ -51,7 +62,7 @@ resource "azurerm_storage_blob" "_05csv" {
   storage_account_name   = azurerm_storage_account.sa.name
   storage_container_name = azurerm_storage_container.raw_entel.name
   type                   = "Block"
-  source                 = "Dataset/05.csv"
+  source                 = "C:/Users/MARVIN/Documents/terraform-sisinfo2-clean/Dataset/05.csv"
 }
 
 resource "azurerm_storage_blob" "_06csv" {
@@ -59,14 +70,12 @@ resource "azurerm_storage_blob" "_06csv" {
   storage_account_name   = azurerm_storage_account.sa.name
   storage_container_name = azurerm_storage_container.raw_entel.name
   type                   = "Block"
-  source                 = "Dataset/06.csv"
+  source                 = "C:/Users/MARVIN/Documents/terraform-sisinfo2-clean/Dataset/06.csv"
 }
 
-
-# base de datos sql
-
+# SQL Server
 resource "azurerm_mssql_server" "db" {
-  name                         = "sql-ucb-sisinfo-entel-delta"
+  name                         = "sql-ucb-sisinfo-entel-delta-${random_integer.suffix.result}"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
@@ -82,28 +91,27 @@ resource "azurerm_mssql_firewall_rule" "rulefirewall" {
 }
 
 resource "azurerm_mssql_database" "dw_entel" {
-  name         = "dw_entel_delta"
-  server_id    = azurerm_mssql_server.db.id
-  collation    = "SQL_Latin1_General_CP1_CI_AS"
-  license_type = "LicenseIncluded"
-  max_size_gb  = 2
-  sku_name     = "S0"
-  enclave_type = "VBS"
+  name                  = "dw_entel_delta"
+  server_id             = azurerm_mssql_server.db.id
+  collation             = "SQL_Latin1_General_CP1_CI_AS"
+  license_type          = "LicenseIncluded"
+  max_size_gb           = 2
+  sku_name              = "S0"
+  enclave_type          = "VBS"
+  storage_account_type  = "Local"  # <- compatible con chilecentral
 
   tags = {
     foo = "bar"
   }
 
-  # prevent the possibility of accidental data loss
   lifecycle {
     prevent_destroy = false
   }
 }
 
-# agregar  data factory
-
+# Data Factory
 resource "azurerm_data_factory" "df" {
-  name                = "adf-ucb-dw-entel-4536427"
+  name                = "adf-ucb-dw-entel-${random_integer.suffix.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
